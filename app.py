4968d3fcf4c139336f289e67d22be5aeb1dbe450
Cheyenne Kim <cheyenne.kim@duke.edu>
2020-03-20 16:58:15 -0400

    random

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = '123'
+app.config.from_object('config')
+db = SQLAlchemy(app, session_options={'autocommit': False})
+
+@app.route('/')
+def all_players():
+    return render_template('first.html')
