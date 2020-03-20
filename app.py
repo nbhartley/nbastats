310e65b0c352c55238842436588d58998823f0c0
Cheyenne Kim <cheyenne.kim@duke.edu>
2020-03-20 17:17:38 -0400

    adsf

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+//from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = '123'
-app.config.from_object('config')
-db = SQLAlchemy(app, session_options={'autocommit': False})
+//app.secret_key = '123'
+//app.config.from_object('config')
+//db = SQLAlchemy(app, session_options={'autocommit': False})
 
 @app.route('/')
 def all_players():
