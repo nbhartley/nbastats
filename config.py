a38a3f5cb06a8bc17547ddd8cc50d24681b9b51c
Cheyenne Kim <cheyenne.kim@duke.edu>
2020-03-22 22:27:47 -0400

    more fixes

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://xgovjbcwxkflgv:e22a9f7f8bf7b708a04f70dbac7ebbbf131283956d4ee8a1cef7cda576c66ae0@ec2-3-234-169-147.compute-1.amazonaws.com:5432/d8fsuve7dqtlcj
-'
+SQLALCHEMY_DATABASE_URI = 'postgres://xgovjbcwxkflgv:e22a9f7f8bf7b708a04f70dbac7ebbbf131283956d4ee8a1cef7cda576c66ae0@ec2-3-234-169-147.compute-1.amazonaws.com:5432/d8fsuve7dqtlcj'
 SQLALCHEMY_ECHO = True
 DEBUG = True
